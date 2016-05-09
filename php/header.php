21b94d5f6f3f5f89e3ccb0c17f88b2bf48085791
Simeron Taak <simeron03@icloud.com>
2016-05-09 23:22:06 +0100

    I have created a php folder and I have made the templates for the php pages. But they are just filled with the name of the page as there is no actual code on these pages. I have only literally copied like typed out the same code which I got from this website, http://blog.spoongraphics.co.uk/tutorials/how-to-build-a-custom-wordpress-theme-from-scratch, But I will probably need to go back and get this I think I'll probably have to change something, but for now I have just type up the code I have seen, and I will try with the rest tomorrow. I have made a new CSS file for the php file I might not need it, and I might be able to use my own but I'll see how it will go.

---
+++ php/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title><?php wp_title('&laguo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/scripts.js"></script>
+
+	<link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" />
+
+	<!-- have midded out a line of code i believe it was for the comments section -->
+
+	<?php wp_head(); ?>
+</head>
+
+<body>
+<div id="wrapper">
+	<div id="container">
+</body>
+
+<!-- I have got all of tis code from this site, I have also not tried to chnage any of the code and I have missed out the comments line -->
