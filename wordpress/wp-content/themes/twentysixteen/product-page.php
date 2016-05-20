206194465bb30174305cecab4d54d8dfe49ae6a9
Simeron Taak <simeron03@icloud.com>
2016-05-20 17:20:22 +0100

    Changed the footer so that it say proudly powered by Simeron, and I tried to make a new template page, I managed to get it to work, but I think I need to get the container CSS because it doesn't look right at the moment.

---
+++ wordpress/wp-content/themes/twentysixteen/product-page.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Template Name: Product page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+<!-- 		go into the skeleton css and copy over any CSS in relation to the container tag, that might be why everything is so big and stretches to each side of the page.  -->
+		
+ <div class="container">
+    <div class="row">
+      <div class="twelve columns">
+        <p>Explore the product category of your choice</p>
+      </div> 
+    </div> 
+    
+    <div class="row">
+      <div class="one-half column " style="margin-top: 5%"> <!-- use to be 25% -->
+        <p>prep and prime</p>
+        <img src="http://www1.macys.com/dyn_img/cat_splash/0203_MAC_main_banner_PrepPrime_1119835.jpg">
+      </div>
+
+       <div class="one-half column " style="margin-top: 5%"> <!-- use to be 25% -->
+        <p>Pimers</p>
+        <img src="http://www1.macys.com/dyn_img/cat_splash/0203_MAC_main_banner_PrepPrime_1119835.jpg">
+      </div>
+    </div> 
+
+
+    <div class="row">
+      <div class="one-half column " style="margin-top: 5%"> <!-- use to be 25% -->
+        <p>Concealer</p>
+       <img src="http://www1.macys.com/dyn_img/cat_splash/0203_MAC_main_banner_PrepPrime_1119835.jpg">
+      </div>
+
+      <div class="one-half column " style="margin-top: 5%"> <!-- use to be 25% -->
+        <p>Foundation</p>
+        <img src="http://www1.macys.com/dyn_img/cat_splash/0203_MAC_main_banner_PrepPrime_1119835.jpg">
+      </div>
+    </div> 
+
+
+    <div class="row">
+      <div class="one-half column " style="margin-top: 5%"> <!-- use to be 25% -->
+        <p>Correctors</p>
+        <img src="http://www1.macys.com/dyn_img/cat_splash/0203_MAC_main_banner_PrepPrime_1119835.jpg">
+      </div>
+
+      <div class="one-half column " style="margin-top: 5%"> <!-- use to be 25% -->
+        <p>Lipstick</p>
+        <img src="http://www1.macys.com/dyn_img/cat_splash/0203_MAC_main_banner_PrepPrime_1119835.jpg">
+      </div>
+    </div> 
+
+
+    <div class="row">
+      <div class="one-half column " style="margin-top: 5%"> <!-- use to be 25% -->
+        <p>Eyes</p>
+        <img src="http://www1.macys.com/dyn_img/cat_splash/0203_MAC_main_banner_PrepPrime_1119835.jpg">
+      </div>
+
+      <div class="one-half column " style="margin-top: 5%"> <!-- use to be 25% -->
+        <p>Mascara</p>
+        <img src="http://www1.macys.com/dyn_img/cat_splash/0203_MAC_main_banner_PrepPrime_1119835.jpg">
+      </div>
+    </div> 
+
+	</main><!-- .site-main -->
+
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
