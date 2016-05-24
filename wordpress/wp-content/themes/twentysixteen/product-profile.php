44648eaa83c721f31ff312be1412448cd277740c
Simeron Taak <simeron03@icloud.com>
2016-05-24 17:12:55 +0100

    Have started work on the product profile page and I need to had to sort the CSS out a little bit.

---
+++ wordpress/wp-content/themes/twentysixteen/product-profile.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Template Name: Product Profile
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+<!-- 		go into the skeleton css and copy over any CSS in relation to the container tag, that might be why everything is so big and stretches to each side of the page.  -->
+		
+  <div class="container">
+
+    <div class="row">
+      <div class="one-half column profile" style="margin-top: 5%"> <!-- use to be 25% -->
+       <img src="https://boots.scene7.com/is/image/Boots/10177346?id=ZzzaM2&fmt=jpg&fit=constrain,1&wid=504&hei=548">
+      </div>
+
+      <div class="one-half column profile" style="margin-top: 5%"> <!-- use to be 25% -->
+        <h5>No.7 Air Brush Away Primer</h5>
+        <p>Primer - This is a product which can be used as a base for makeup. Or you can also wear it alone, to hide pores and leave you skins with a soft velvety finish. Perfect for all skin types, from dry to oily, and also doesn't block pores. This primer inparticular also has skin care properties, so will also help with fine lines, creases and wrinkes.</p>
+        <strong>30mls</strong>
+        <br>
+        <strong>£16.50 - £20.00</strong>
+      </div>
+    </div> 
+
+    <hr>
+
+
+    <div class="row">
+      <div class="one-half column " style="margin-top: 5%"> <!-- use to be 25% -->
+        <h5>Customer Reviews</h5>
+        <hr>
+      </div>
+
+      <div class="one-half column " style="margin-top: 5%"> <!-- use to be 25% -->
+        <h5>Expert Reviews</h5>
+        <hr>
+      </div>
+
+    </div> 
+
+
+    <div class="row">
+      <div class="two columns" style="margin-top: 5%"> <!-- use to be 25% -->
+        <img src="http://www.sephora.com/productimages/sku/s1343938-main-zoom.jpg">
+    
+      </div>
+
+      <div class="ten columns text" style="margin-top: 5%"> <!-- use to be 25% -->
+         <h6><strong>There real mascara</strong></h6>
+        <p>I just wanted to write this review about this product, which is the Benefit they're real mascara. It has a really nice wand a and the mascara itself lasts a long time, it is also quite a stubborn to take off as well, when though it's not waterproof. </p>
+      </div>
+    </div> 
+
+
+    <div class="row">
+      <div class="two columns" style="margin-top: 5%"> <!-- use to be 25% -->
+        <img src="http://www.sephora.com/productimages/sku/s1343938-main-zoom.jpg">
+      </div>
+
+      <div class="ten columns text" style="margin-top: 5%"> <!-- use to be 25% -->
+        <h6><strong>There real mascara</strong></h6>
+        <p>I just wanted to write this review about this product, which is the Benefit they're real mascara. It has a really nice wand a and the mascara itself lasts a long time, it is also quite a stubborn to take off as well, when though it's not waterproof. </p>
+      </div>
+    </div> 
+
+    <br>
+
+   <!--  <hr>
+
+    <h5>Write your first review!</h5> 
+
+    <hr> -->
+
+  </div> 
+
+	</main><!-- .site-main -->
+
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
