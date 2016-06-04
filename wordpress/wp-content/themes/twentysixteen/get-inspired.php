c32f3721a50b4860999737c73dd5a9c6144128da
Simeron Taak <simeron03@icloud.com>
2016-06-04 15:33:41 +0100

    Added links to three products on the get inspired page. Sleek still to do.

--- wordpress/wp-content/themes/twentysixteen/get-inspired.php
+++ wordpress/wp-content/themes/twentysixteen/get-inspired.php
@@ -21,7 +21,7 @@ get_header(); ?>
     
     <div class="row">
       <div class="five columns" style="margin-top: 5%"> <!-- use to be 25% -->
-        <p>Maybelline Push Up Drama Mascara Very Black</p>
+        <p><a href="<?php echo home_url(); ?>/product/maybelline-push-up-drama-mascara-very-black/">Maybelline Push Up Drama Mascara Very Black</a></p>
         <img src="<?php echo get_template_directory_uri(); ?>/images/maybelline-push-up.jpg" width="100%";>
       </div>
 
@@ -33,12 +33,12 @@ get_header(); ?>
 
     <div class="row">
       <div class="seven columns"style="margin-top: 5%"> <!-- use to be 25% -->
-        <p>Urban Decay Naked Palette</p>
+        <p><a href="<?php echo home_url(); ?>/product/urban-decay-naked3-eyeshadow-palette/">Urban Decay Naked3 Eyeshadow Palette</a></p>
        <img src="<?php echo get_template_directory_uri(); ?>/images/naked-urban-decay.jpg" width="100%">
       </div>
 
       <div class="five columns" style="margin-top: 5%"> <!-- use to be 25% -->
-        <p>Rimmel Lipstick</p>
+        <p><a href="<?php echo home_url(); ?>/product/rimmel-lasting-finish-lipstick/">Rimmel Lasting Finish Lipstick</a></p>
         <img src="<?php echo get_template_directory_uri(); ?>/images/rimmell-lipstick.jpg" width="100%">
       </div>
     </div> 
