daac8e514cdf5ab786f05b4c780c2d38cc6c313e
Simeron Taak <simeron03@icloud.com>
2016-08-27 18:43:10 +0100

    A lot of changes where made here, I'm not gunna lie I can't remember all of them, I think a lot of them had to do with the plugins, and making backups. I have also moved the folder and I have only seen these commits now.

---
+++ wordpress/wp-content/plugins/all-in-one-wp-migration/storage/status.js
@@ -0,0 +1,1 @@
+{"type":"download","message":"<a href=\"http:\/\/localhost:8888\/wp-content\/ai1wm-backups\/localhost-20160627-120703-228.wpress\" class=\"ai1wm-button-green ai1wm-emphasize\"><span>Download localhost<\/span><em>Size: 47 MB<\/em><\/a>","title":null}
